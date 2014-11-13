c746a7801ef6232ba82727ec42304d8632d71ee0
jh69 <jonhendren@gmail.com>
2014-11-13 11:18:23 -0800

    Update mt.py

--- mt.py
+++ mt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys
+import sys, time
 skull = """      .ed\"\"\"\" \"\"\"\$$$$be.
      -\"           ^\"\"**$$$e.
    .\"                   \'$$$c
@@ -23,12 +23,37 @@ $$$$P d$$$$F $ $$$$$$$$$- $$$$$$
        $$$e   z$$$$$$$$$$ 
         \"*$c  \"$$$$$$$P\"
           \"\"\"*$$$$$$$\""""
+skulllaugh = """      .ed\"\"\"\" \"\"\"\$$$$be.
+     -\"           ^\"\"**$$$e.
+   .\"                   \'$$$c
+  /                      \"4$$b
+ d  3                      $$$$
+ $  *                   .$$$$$$
+.$  ^c           $$$$$e$$$$$$$$.
+d$L  4.         4$$$$$$$$$$$$$$b
+$$$$b ^ceeeee.  4$$ECL.F*$$$$$$$
+$$$$P d$$$$F $ $$$$$$$$$- $$$$$$
+3$$$F \"$$$$b   $\"$$$$$$$  $$$$*\"
+ $$P\"  \"$$b   .$ $$$$$...e$$
+  *c    ..    $$ 3$$$$$$$$$$eF
+    %ce\"\"    $$$  $$$$$$$$$$*
+     *$e.    *** d$$$$$\"L$$
+      $$$      4J$$$$$% $$$
+     $\"\'$=e....$*$$**$cz$$\"
+     $$$                 $$
+      $$$              $$$
+     $  *=%4.$ L L$ P3$$$F
+     $   \"%*ebJLzb$e$$$$$b
+      %..      4$$$$$$$$$$
+       $$$e   z$$$$$$$$$$ 
+        \"*$c  \"$$$$$$$P\"
+          \"\"\"*$$$$$$$\""""
+crack = "\n       PROGRAM IS CRACK BY" + "\n    HACK SQUAD CHECKMATE 1992" + "\n        SERBIA BEST NATION\n"
 mt = 69
 if len(sys.argv) > 1 and sys.argv[1].isdigit():
 	mt = int(sys.argv[1])
 for i in range(mt):
-	print "Mmmm... Toasty"
-print
-print skull
-print "\n       PROGRAM IS CRACK BY" + "\n    HACK SQUAD CHECKMATE 1992" + "\n        SERBIA BEST NATION\n"
-	
+	print skull, "\n          Mmmm... Toasty",crack
+	time.sleep(0.5)
+	print skulllaugh, "\n          Mmmm... Toasty",crack
+	time.sleep(0.5)

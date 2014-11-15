624fff9678353fd48701ed978b16d69976b856f6
Jon Billingsley <jon.billingsley@gettyimages.com>
2014-11-15 14:12:25 -0700

    remove trailing whitespace, fix indentation to 4 spaces

--- mt.py
+++ mt.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import sys, time, os, random
 class color:
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   END = '\033[0m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    END = '\033[0m'
 skull = """      .ed\"\"\"\" \"\"\"\$$$$be.
      -\"           ^\"\"**$$$e.
    .\"                   \'$$$c
@@ -24,7 +24,7 @@ $$$$P d$$$$F $ $$$$$$$$$- $$$$$$
      $  *=%4.$ L L$ P3$$$F
      $   \"%*ebJLzb$e$$$$$b
       %..      4$$$$$$$$$$
-       $$$e   z$$$$$$$$$$ 
+       $$$e   z$$$$$$$$$$
         \"*$c  \"$$$$$$$P\"
           \"\"\"*$$$$$$$\""""
 skulllaugh = """      .ed\"\"\"\" \"\"\"\$$$$be.
@@ -49,26 +49,26 @@ $$$$P d$$$$F $ $$$$$$$$$- $$$$$$
      $  *=%4.$ L L$ P3$$$F
      $   \"%*ebJLzb$e$$$$$b
       %..      4$$$$$$$$$$
-       $$$e   z$$$$$$$$$$ 
+       $$$e   z$$$$$$$$$$
         \"*$c  \"$$$$$$$P\"
           \"\"\"*$$$$$$$\""""
 crack = "\n       PROGRAM IS CRACK BY" + "\n    HACK SQUAD CHECKMATE 1992" + "\n        SERBIA BEST NATION\n"
 mt = 69
 if len(sys.argv) > 1 and sys.argv[1].isdigit():
-  mt = int(sys.argv[1])
+    mt = int(sys.argv[1])
 for i in range(25):
-	print '\033[1;37;' + str(random.randint(41,46)) + 'm' + (" " * 50)
-	time.sleep(0.05)
-	if i == 12: print ("\033[1;37;41m###############   YOU     ARE  ###################")
-	if i == 13: print ("\033[1;37;41m##############   MOTHER FUCKER   #################")
-print '\033[0m'	
+    print '\033[1;37;' + str(random.randint(41,46)) + 'm' + (" " * 50)
+    time.sleep(0.05)
+    if i == 12: print ("\033[1;37;41m###############   YOU     ARE  ###################")
+    if i == 13: print ("\033[1;37;41m##############   MOTHER FUCKER   #################")
+print '\033[0m'
 time.sleep(1)
 os.system('clear')
 
 for i in range(mt):
-  print color.RED + skull  + color.END + "\n          Mmmm... Toasty" + crack
-  time.sleep(0.4)
-  os.system('clear')
-  print color.RED + skulllaugh  + color.END + "\n          Mmmm... Toasty" + crack
-  time.sleep(0.4)
-  os.system('clear')
+    print color.RED + skull  + color.END + "\n          Mmmm... Toasty" + crack
+    time.sleep(0.4)
+    os.system('clear')
+    print color.RED + skulllaugh  + color.END + "\n          Mmmm... Toasty" + crack
+    time.sleep(0.4)
+    os.system('clear')

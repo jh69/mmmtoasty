6d109700b529875b89401e40d3c8dcac9495b881
jh69 <jh69@users.noreply.github.com>
2014-11-15 20:30:41 -0800

    streamlined laughing skull, added credits

--- mt.py
+++ mt.py
@@ -20,38 +20,24 @@ $$$$P d$$$$F $ $$$$$$$$$- $$$$$$
     %ce\"\"    $$$  $$$$$$$$$$*
      *$e.    *** d$$$$$\"L$$
       $$$      4J$$$$$% $$$
-     $\"\'$=e....$*$$**$cz$$\"
+     $\"\'$=e....$*$$**$cz$$\""""
+laugh = """
+     $$$                $$
+      $$$             $$$$"""
+jaw = """
      $  *=%4.$ L L$ P3$$$F
      $   \"%*ebJLzb$e$$$$$b
       %..      4$$$$$$$$$$
        $$$e   z$$$$$$$$$$
         \"*$c  \"$$$$$$$P\"
           \"\"\"*$$$$$$$\""""
-skulllaugh = """      .ed\"\"\"\" \"\"\"\$$$$be.
-     -\"           ^\"\"**$$$e.
-   .\"                   \'$$$c
-  /                      \"4$$b
- d  3                      $$$$
- $  *                   .$$$$$$
-.$  ^c           $$$$$e$$$$$$$$.
-d$L  4.         4$$$$$$$$$$$$$$b
-$$$$b ^ceeeee.  4$$ECL.F*$$$$$$$
-$$$$P d$$$$F $ $$$$$$$$$- $$$$$$
-3$$$F \"$$$$b   $\"$$$$$$$  $$$$*\"
- $$P\"  \"$$b   .$ $$$$$...e$$
-  *c    ..    $$ 3$$$$$$$$$$eF
-    %ce\"\"    $$$  $$$$$$$$$$*
-     *$e.    *** d$$$$$\"L$$
-      $$$      4J$$$$$% $$$
-     $\"\'$=e....$*$$**$cz$$\"
-     $$$                 $$
-      $$$              $$$
-     $  *=%4.$ L L$ P3$$$F
-     $   \"%*ebJLzb$e$$$$$b
-      %..      4$$$$$$$$$$
-       $$$e   z$$$$$$$$$$
-        \"*$c  \"$$$$$$$P\"
-          \"\"\"*$$$$$$$\""""
+credits = ["","","","","","","","","","","","","","","","","","","","","","","","","",
+"CREDITS"," ","MMM... TOASTY"," ","BY"," ","BEST NATION SERBIA WARRIORS"," ",
+"WE CREATE HELL FOR CROAT"," ","FUCK TO CROAT SWINE"," ","SERBIA NUMBER 1"," ","HACK SQUAD CHECKMATE 1992",
+" ","GREETINGS TO:"," ","HACK BABY, DIGITAL MAN 1994, MR. FAT, TONY,",
+"TRANCE PRINCE, GHOST WOMAN, BIO HOG, TAD"," ","FUCK TO CROAT !! IT IS A SHIT TO ME",
+"","","","","","","","","","","","","","","","","","","","","","","","",""]
+
 crack = "\n       PROGRAM IS CRACK BY" + "\n    HACK SQUAD CHECKMATE 1992" + "\n        SERBIA BEST NATION\n"
 mt = 69
 if len(sys.argv) > 1 and sys.argv[1].isdigit():
@@ -66,9 +52,21 @@ time.sleep(1)
 os.system('clear')
 
 for i in range(mt):
-    print color.RED + skull  + color.END + "\n          Mmmm... Toasty" + crack
-    time.sleep(0.4)
-    os.system('clear')
-    print color.RED + skulllaugh  + color.END + "\n          Mmmm... Toasty" + crack
-    time.sleep(0.4)
-    os.system('clear')
+	if i % 2 == 0:
+		print color.RED + skull + jaw + color.END + "\n          Mmmm... Toasty" + crack
+		time.sleep(0.4)
+		os.system('clear')
+	else:
+		print color.RED + skull + laugh + jaw + color.END + "\n          Mmmm... Toasty" + crack
+		time.sleep(0.4)
+		os.system('clear')
+		
+while len(credits) > 1:
+	for i in range(25):
+		if len(credits) < 25:
+			exit()
+		#print len(credits), " ", i
+		print credits[i]
+	time.sleep(0.2)
+	os.system('clear')
+	credits.pop(0)
